5373410fe03e08ce47c95302ef6694d989e75ed6
Kristina <kmatuleviciute@tssg.org>
2017-05-05 10:12:22 +0100

    update, delete, create user

--- api/users/index.js
+++ api/users/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import users from './users';
+import _ from 'lodash';
 
 const router = express.Router();
 
@@ -10,5 +11,48 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   res.send({ users: users });
 });
+
+router.post('/', (req, res) => {
+        let newUser = req.body;
+        if (newUser){
+          users.push({name: newUser.name, address : newUser.address, phone_number: newUser.phone_number }) ;
+          res.status(201).send({message: "User Created"});
+      }else{
+            res.status(400).send({message: "Unable to find User in request. No User Found in body"});
+      }
+});
+//Update a user
+router.put('/:id', (req, res) => {
+     let key = req.params.id;
+     let updateUser = req.body;
+     var index = _.findIndex(users, user => {
+                 return user.phone_number === key;
+              });
+            if (index !== -1) {
+               users.splice(index, 1, {name: updateUser.name, address: updateUser.address, phone_number: updateUser.phone_number});
+               res.status(200).send({message: "User Updated"});
+              }
+              else{
+          res.status(400).send({message: "Unable to find User in request. No User Found in body"}) ;
+      }
+});
+
+//Delete a user
+router.delete('/:id', (req, res) => {
+     let key = req.params.id;
+   var elements = _.remove(users,
+              user => {
+                     return user.phone_number === key;
+                  });
+    if (elements){
+       res.status(200).send({message: "User deleted"});
+    }else{
+      res.status(400).send({message: "Unable to find user. No user Deleted"}) ;
+      }
+
+
+});
+
+
 
 export default router;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import config from './config';
 import express from 'express';
 import usersRouter from './api/users';
+import bodyParser from 'body-parser';
 
 const server = express();
 
+//configure body-parser
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded());
 server.use('/api/users', usersRouter);
 server.use(express.static('public'));
 
